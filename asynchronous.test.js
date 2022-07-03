c1d2e81b972d1036f0cb83bcd722bb76c5408061
HUANG-SIH-MAN <cindy20303705@gmail.com>
2022-07-04 07:38:11 +0800

    add mock function unit test

--- asynchronous.test.js
+++ asynchronous.test.js
@@ -10,31 +10,31 @@ const addNumber =	function (a, b) {
 describe("asychronous fuction", () => {
   it("async/await", async () => {
     let result = await addNumber("a", 1).catch(msg => msg)
-    expect(result).toMatch("type error");
+    expect(result).toMatch("type error")
 
-    await expect(addNumber(1, 1)).resolves.toBe(2);
-    await expect(addNumber("a", 1)).rejects.toMatch("type error");
+    await expect(addNumber(1, 1)).resolves.toBe(2)
+    await expect(addNumber("a", 1)).rejects.toMatch("type error")
   })
 
   test('try/catch', async () => {
     try {
       await addNumber("a", 1)
     } catch (err) {
-      expect(err).toMatch("type error");
+      expect(err).toMatch("type error")
     }
   });
 
   test('promise resolve', () => {
-    return expect(addNumber(1, 1)).resolves.toBe(2);
+    return expect(addNumber(1, 1)).resolves.toBe(2)
   });
 
   test('promise rejects', () => {
-    return expect(addNumber("a", 1)).rejects.toMatch("type error");
+    return expect(addNumber("a", 1)).rejects.toMatch("type error")
   });
 
   test("then", () => {
     return addNumber(1, 1).then(result => {
-      expect(result).toBe(2);
+      expect(result).toBe(2)
     });
   })
 })
--- mock.test.js
+++ mock.test.js
@@ -1,20 +1,21 @@
 const axios = require("axios")
 
+class Users {
+  static all() {
+    return axios.get('/users.json').then(resp => resp.data);
+  }
+}
+
+// 將整組的 axios 套件方法 mock (不能寫在test內，會出現錯誤)
+jest.mock('axios');
+
+
 const add = function (a, b) {
   return a + b
 }
 
-const addNumber =	function (a, b) {
-  return new Promise((resolve, reject) => {
-    if (isNaN(a) || isNaN(b)) {
-      reject("type error")
-    }
-    resolve(a + b)
-  })
-}
-
 describe("mock function", () => {
-  it("mock function", () => {
+  it("mock function set return value", () => {
     // 沒有被 mock 
     expect(add(1, 1)).toBe(2)
 
@@ -30,6 +31,38 @@ describe("mock function", () => {
     expect(mockFunction(1, 1)).toBe(100)
   })
 
+  it("mock function get information", () => {
+    const mockFunction = jest.fn(add)
+    mockFunction(1, 2)
+    mockFunction(2, 3)
+    mockFunction(3, 4)
+    expect(mockFunction.mock.calls.length).toBe(3); // 確認函式被呼叫的次數
+    expect(mockFunction.mock.calls[0][0]).toBe(1); // 確認函式被呼叫時傳入的參數(第一次、第一個參數)
+    expect(mockFunction.mock.calls[1][0]).toBe(2); // 確認函式被呼叫時傳入的參數(第二次、第一個參數)
+    expect(mockFunction.mock.results[2].value).toBe(7); // 確認函式被呼叫時的回傳值(第三次呼叫)
+    console.log("calls", mockFunction.mock.calls)  // mock.calls 會記錄傳入的參數
+    console.log("results", mockFunction.mock.results) //mock.results 會記錄函式的回傳值
+    console.log("invocationCallOrder", mockFunction.mock.invocationCallOrder) // 回傳記錄了函式呼叫次數的陣列，用陣列長度來計算呼叫次數
+  })
 
+  it("mock module", async () => {
+    const res = { data: "goods" }
+
+    // 為 axios 中的 get 模擬回傳值為 res
+    axios.get.mockResolvedValue(res)
+    await expect(axios.get('url/allGoods')).resolves.toEqual(res)
+  })
+
+  test('should fetch users，官方範例', () => {
+
+    const users = [{name: 'Bob'}];
+    const resp = {data: users};
+    axios.get.mockResolvedValue(resp);
+  
+    // or you could use the following depending on your use case:
+    // axios.get.mockImplementation(() => Promise.resolve(resp))
+  
+    return Users.all().then(data => expect(data).toEqual(users));
+  });
 
 })
